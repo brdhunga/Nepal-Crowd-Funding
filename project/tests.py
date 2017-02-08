04908e9b6640ab5460623b45d9cf9d0fbf1ab9b4
Bivu Raj Dhungana <brdhunga@gmail.com>
2017-02-08 14:13:04 -0600

    Project: test to check unique slugs passed

--- project/tests.py
+++ project/tests.py
@@ -24,6 +24,14 @@ class ProjectTest(TestCase):
             location="Kathmandu", description="Project description",
             end_date = six_months, created_by = user
             )
+        self.proj2.save()
+
+        self.proj2_duplicate = Project(funding_goal=90000.00, 
+            title="Test title 1", tagline="The best project",
+            location="Kathmandu", description="Project description",
+            end_date = six_months, created_by = user
+            )
+        self.proj2_duplicate.save()
 
     def test_cannot_save_without_user(self):
         with self.assertRaises(django.db.utils.IntegrityError):
@@ -31,3 +39,11 @@ class ProjectTest(TestCase):
 
     def test_saves_project_with_no_error(self):
         self.assertEqual(self.proj2.project_status, Project.DRAFTED)
+
+    def test_slug_not_empty(self):
+        self.assertEqual(self.proj2.slug, "test-title-1")
+
+    def test_slugs_are_unique(self):
+        self.assertNotEqual(self.proj2.slug, self.proj2_duplicate.slug)
+
+
